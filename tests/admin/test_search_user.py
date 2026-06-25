0fc2d77b3cd86564c1d2eb846b939347f9fb1d33
shivanid29 <shivanidesai2930@gmail.com>
2026-06-25 13:58:11 +0530

    fixing the search test

--- tests/admin/test_search_user.py
+++ tests/admin/test_search_user.py
@@ -5,15 +5,14 @@ from utils.config import USERNAME,PASSWORD
 
 
 def test_search_existing_user(page):
+
     login = LoginPage(page)
     admin = AdminPage(page)
 
-    try:
-        login.open()
-        login.login(USERNAME, PASSWORD)
-        admin.open_admin()
-        admin.search_user("Admin")
-        assert admin.search_result_exists()
-    except Exception:
-        admin.take_screenshot("search_user_failure")
-        raise
+    login.open()
+    login.login(USERNAME, PASSWORD)
+
+    admin.open_admin()
+    admin.search_user("Admin")
+
+    assert admin.search_result_exists()
