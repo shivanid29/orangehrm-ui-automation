b3584c068df222c3e3ddd4069c7eab5d16f910d5
shivanid29 <shivanidesai2930@gmail.com>
2026-06-25 16:57:37 +0530

    fix: updated the test to headless mode

--- conftest.py
+++ conftest.py
@@ -32,7 +32,7 @@ def base_url(env):
 @pytest.fixture
 def page(base_url):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 
